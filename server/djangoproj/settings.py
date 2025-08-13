5b293e96bcf253dab4fe2e814fa51cf586cdc0d4
Nerf-Ivan <ivan.swanepoel.dev@gmail.com>
2025-08-13 06:13:11 -0400

    fixed allowed hosts

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['loacalhost', 'https://ivanswanepoe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://ivanswanepoe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'https://ivanswanepoe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000','https://ivanswanepoe-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
