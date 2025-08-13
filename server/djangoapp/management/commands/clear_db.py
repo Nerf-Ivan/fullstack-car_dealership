ee3457a2a27251e6b8bcf66f9384292b70be83ba
Nerf-Ivan <ivan.swanepoel.dev@gmail.com>
2025-08-13 16:03:08 +0200

    populate db

---
+++ server/djangoapp/management/commands/clear_db.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from djangoapp.models import CarMake, CarModel
+
+class Command(BaseCommand):
+    help = 'Clears all car make and car model data from the database'
+
+    def handle(self, *args, **kwargs):
+        try:
+            car_make_count = CarMake.objects.count()
+            car_model_count = CarModel.objects.count()
+            
+            CarMake.objects.all().delete()
+            CarModel.objects.all().delete()
+            
+            self.stdout.write(
+                self.style.SUCCESS(f'Successfully cleared {car_make_count} car makes and {car_model_count} car models from the database')
+            )
+        except Exception as e:
+            self.stdout.write(
+                self.style.ERROR(f'Error clearing database: {str(e)}')
+            )
